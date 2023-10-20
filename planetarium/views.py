0fc9ad7b9a64a2304c0620988d924866ccaea79f
Yeva <yevabrookman@gmail.com>
2023-10-20 14:37:27 +0300

    Added views for planetarium app

--- planetarium/views.py
+++ planetarium/views.py
@@ -1,3 +1,97 @@
-from django.shortcuts import render
+from django.db.models import F, Count
+from rest_framework import viewsets, mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from planetarium.models import (
+    ShowTheme,
+    AstronomyShow,
+    PlanetariumDome,
+    Reservation,
+    ShowSession,
+)
+from planetarium.serializers import (
+    ShowThemeSerializer,
+    AstronomyShowSerializer,
+    PlanetariumDomeSerializer,
+    ReservationSerializer,
+    ShowSessionSerializer,
+    AstronomyShowListSerializer,
+    AstronomyShowDetailSerializer,
+    ShowSessionListSerializer,
+    ReservationListSerializer,
+)
+
+
+class ShowThemeViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    serializer_class = ShowThemeSerializer
+    queryset = ShowTheme.objects.all()
+
+
+class AstronomyShowViewSet(viewsets.ModelViewSet):
+    serializer_class = AstronomyShowSerializer
+    queryset = AstronomyShow.objects.all()
+
+    @staticmethod
+    def _params_to_ints(qs):
+        """Converts a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in qs.split(",")]
+
+    def get_queryset(self):
+        show_themes = self.request.query_params.get("show_themes")
+        queryset = self.queryset
+
+        if show_themes:
+            show_themes_id = self._params_to_ints(show_themes)
+            queryset = queryset.filter(show_themes__id__in=show_themes_id)
+
+        return queryset.distinct()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return AstronomyShowListSerializer
+        if self.action == "retrieve":
+            return AstronomyShowDetailSerializer
+
+        return AstronomyShowSerializer
+
+
+class ShowSessionViewSet(viewsets.ModelViewSet):
+    serializer_class = ShowSessionSerializer
+    queryset = (
+        ShowSession.objects.all()
+        .select_related("astronomy_show", "planetarium_dome")
+        .annotate(
+            tickets_available=(
+                F("planetarium_dome__rows") * F("planetarium_dome__seats_in_row")
+                - Count("tickets")
+            )
+        )
+    )
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ShowSessionListSerializer
+        return ShowSessionSerializer
+
+
+class PlanetariumDomeViewSet(viewsets.ModelViewSet):
+    serializer_class = PlanetariumDomeSerializer
+    queryset = PlanetariumDome.objects.all()
+
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    serializer_class = ReservationSerializer
+    queryset = Reservation.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ReservationListSerializer
+        return ReservationSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
