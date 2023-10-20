5d5d93eb8b1c1b9aaa6ac8253c34ef8060fc2993
Yeva <yevabrookman@gmail.com>
2023-10-20 14:38:32 +0300

    Added urls for views in planetarium app

--- planetarium/urls.py
+++ planetarium/urls.py
@@ -1,1 +1,23 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from planetarium.views import (
+    ShowThemeViewSet,
+    AstronomyShowViewSet,
+    ReservationViewSet,
+    PlanetariumDomeViewSet,
+    ShowSessionViewSet,
+)
+
+router = routers.DefaultRouter()
+router.register("show_themes", ShowThemeViewSet)
+router.register("astronomy_shows", AstronomyShowViewSet)
+router.register("planetarium_domes", PlanetariumDomeViewSet)
+router.register("reservations", ReservationViewSet)
+router.register("astronomy_shows", AstronomyShowViewSet)
+router.register("show_sessions", ShowSessionViewSet)
+
+
+urlpatterns = [path("", include(router.urls))]
+
 app_name = "planetarium"
